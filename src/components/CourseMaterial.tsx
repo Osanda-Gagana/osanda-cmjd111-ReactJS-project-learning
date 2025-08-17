191ff6ff03e8f09ff166157709a6027ee26f3c64
OsandaGagana <Osandagds@gmail.com>
2025-08-17 13:37:16 +0530

    adding table and entries

--- src/components/CourseMaterial.tsx
+++ src/components/CourseMaterial.tsx
@@ -1,7 +1,32 @@
-export const CourseMaterial = () => {
+import { Table } from "react-bootstrap";
+
+export const CourseMaterial = () =>{
     return(
         <>
-        <h1>Cm</h1>
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Material ID</th>
+          <th>File Name</th>
+          <th>Material Name</th>
+          <th>Material</th>
+          <th>Upload Time</th>
+          <th>Course ID</th>
+          <th>Option</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </Table>
         </>
     );
-}
+} 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ export const NavBar = ()=>{
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">Course Material</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
